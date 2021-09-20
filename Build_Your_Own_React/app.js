46afa8484628f5e4a32dfd36c14f30c03695dea9
Ranja Func <seolin9340@gmail.com>
2021-09-20 21:39:27 +0900

    Study React render

--- Build_Your_Own_React/app.js
+++ Build_Your_Own_React/app.js
@@ -62,3 +62,23 @@ const element = Didact.createElement(
         </div>
     )
 */
+
+
+function render(element, container) {
+    const dom = element.type === "TEXT_ELEMENT"
+    ? document.createTextNode("")
+    : document.createElement(element.type);
+
+    const isProperty = (key) => key !== "children";
+    Object.keys(element.props)
+    .filter(isProperty)
+    .forEach(name => {
+        dom[name] = element.props[name];
+    })
+
+    element.props.children.forEach(child => 
+        render(child, dom)
+    )
+
+    container.appendChild(dom);
+}
